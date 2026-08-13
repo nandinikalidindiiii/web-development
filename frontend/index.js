d2a900edefc8ba95a0a5b8713e6d915e7ffa7e4f
nandinikalidindiiii <nandinikalidindi07@gmail.com>
2026-08-13 16:25:06 +0530

    toggle changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,26 @@
-alert("welcome to web bootcamp")
+var user = [
+    {
+    "name": "John Doe",
+    "gender": "male",
+    "image":"john.png"
+    },
+    {
+     "name": "Jane",
+    "gender": "female",
+    "image":"jane.png"
+
+}
+
+]
+ var curId = 0;
+  function toggleUser() {
+    curId = (curId + 1) % 2;
+
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+    var userImage = document.getElementById("user-image");
+
+    userName.innerHTML = user[curId].name;
+    userGender.innerHTML = user[curId].gender;
+    userImage.src = user[curId].image;
+  }
